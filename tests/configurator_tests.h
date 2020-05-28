c51edcd1c0ee11296bad7e79ab2855973dd8b32f
NikonP <podgorny.nikon@yandex.ru>
2020-05-28 19:27:03 +0300

    saveConfig & loadConfig tests

--- tests/configurator_tests.h
+++ tests/configurator_tests.h
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 #include <QDir>
+#include <QFileInfo>
 #include "configurator.h"
 
 using namespace testing;
@@ -16,11 +17,37 @@ TEST(init_dirs, check_dirs)
     EXPECT_EQ(true, QDir(cfg.audioDirPath).exists());
 }
 
-TEST(init_config, check_config) {
+TEST(init_config, check_data) {
     Configurator cfg;
     cfg.initDirs();
-    cfg.initConfig();
+    cfg.initConfig(); // load default
     Configurator::ConfigStorage config = cfg.getConfig();
+    for(auto key : cfg.defaultConfig.keys()) {
+        EXPECT_EQ(1, config.keys().contains(key));
+        EXPECT_EQ(1, config[key].length());
+        EXPECT_EQ(1, config[key].contains(cfg.defaultConfig[key]));
+    }
+}
+
+TEST(save_config, check_file) {
+    Configurator cfg;
+    cfg.initDirs();
+    cfg.initConfig(); // load default
+    cfg.saveConfig(); // save deafult
+
+    QFileInfo test_file(cfg.configFilePath);
+    EXPECT_EQ(1, test_file.exists());
+    EXPECT_EQ(1, test_file.isFile());
+}
+
+TEST(load_config, check_data) {
+    Configurator cfg;
+    cfg.initDirs();
+    cfg.initConfig(); // load default
+    cfg.saveConfig(); // save deafult
+
+    Configurator::ConfigStorage config = cfg.getConfig();
+    EXPECT_EQ(1, config.keys().length() != 0);
     for(auto key : cfg.defaultConfig.keys()) {
         EXPECT_EQ(1, config.keys().contains(key));
         EXPECT_EQ(1, config[key].length());
