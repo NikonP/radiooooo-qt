aae1f55912353429f8cdffe1afd619609fae3450
NikonP <podgorny.nikon@yandex.ru>
2020-05-28 19:09:45 +0300

    initConfig test

--- tests/configurator_tests.h
+++ tests/configurator_tests.h
@@ -16,4 +16,15 @@ TEST(init_dirs, check_dirs)
     EXPECT_EQ(true, QDir(cfg.audioDirPath).exists());
 }
 
+TEST(init_config, check_config) {
+    Configurator cfg;
+    cfg.initDirs();
+    cfg.initConfig();
+    Configurator::ConfigStorage config = cfg.getConfig();
+    for(auto key : cfg.defaultConfig.keys()) {
+        EXPECT_EQ(1, config.keys().contains(key));
+        EXPECT_EQ(1, config[key].contains(cfg.defaultConfig[key]));
+    }
+}
+
 #endif // CONFIGURATOR_TESTS_H
