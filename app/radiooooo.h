82d35dba5b6de1f0d48c06504829920284785af9
NikonP <podgorny.nikon@yandex.ru>
2021-04-18 21:35:16 +0300

    wtf

--- app/radiooooo.h
+++ app/radiooooo.h
@@ -152,8 +152,8 @@ private:
         bool enable);
 
         /**
-        * @brief Изменяет состояние аудиоплеера
-        * @param play - запустить (true) или остановить (false)
+        * /brief Изменяет состояние аудиоплеера
+        * /param play - запустить (true) или остановить (false)
         */
         void playTrigger(bool play);
         /**
